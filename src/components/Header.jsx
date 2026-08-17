abeeeaf90760c2c36063adc5e20d3341493ec1c1
Teodoro Colella <colella.teo@gmail.com>
2026-08-16 21:43:15 -0700

    Added pages utilizing rReact Router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+
 function Header({tagline}) {
     return (
         <div>
             <h1>Mindscape</h1>
             <p>{tagline}</p>
+            <nav>
+                <ul>
+                    <li><Link to="/">Check In</Link></li>
+                    <li><Link to="/history">History</Link></li>
+                    <li><Link to="/breathe">Breathe</Link></li>
+                    <li><Link to="/resources">Resources</Link></li>
+                </ul>
+            </nav>
         </div>
 
     )
